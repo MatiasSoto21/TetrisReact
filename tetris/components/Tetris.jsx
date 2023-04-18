a0bf02f6abad766044f025dc6faeabb2955b886d
MatiasSoto21 <matiassoto00@outlook.com>
2023-04-18 19:11:50 -0300

    custom hooks update for tetromino

--- tetris/components/Tetris.jsx
+++ tetris/components/Tetris.jsx
@@ -5,18 +5,49 @@ import StartButton from "./StartButton";
 import styles from "../modulecss/Tetris.module.css";
 import { usePlayer } from "@/utils/usePlayer";
 import { useStage } from "@/utils/useStage";
+import { createStage } from "@/utils/gameHelpers";
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   console.log("re-render");
 
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0});
+  }
+
+  const startGame = () => {
+    //reset
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+  const move = ({keyCode}) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer()
+      }
+    }
+  }
+
   return (
-    <div id={styles.main} className="w-screen h-screen overflow-hidden">
+    <div id={styles.main} className="w-screen h-screen overflow-hidden" /* role="button" */ tabIndex="0" onKeyDown={e => move(e)} >
       <div className="flex items-start p-[40px] ml-[200px] m-0 max-w-[950px] h-[1000px]">
         <Stage stage={stage} />
         <aside className=" w-full max-w-[280px] block p-2">
@@ -29,7 +60,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame}/>
         </aside>
       </div>
     </div>
