d034d6ec70207e302030cae1857b91087d6a40ba
MatiasSoto21 <matiassoto00@outlook.com>
2023-04-18 00:13:06 -0300

    custom hooks

--- tetris/components/Tetris.jsx
+++ tetris/components/Tetris.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
-import { createStage } from "@/utils/gameHelpers";
-import styles from "../modulecss/Tetris.module.css"
-import bgImage from "../public/img/bg.png";
+import styles from "../modulecss/Tetris.module.css";
+import { usePlayer } from "@/utils/usePlayer";
+import { useStage } from "@/utils/useStage";
 
 const Tetris = () => {
-  console.log(createStage());
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("re-render");
+
   return (
     <div id={styles.main} className="w-screen h-screen overflow-hidden">
       <div className="flex items-start p-[40px] ml-[200px] m-0 max-w-[950px] h-[1000px]">
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside className=" w-full max-w-[280px] block p-2">
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
           <StartButton />
         </aside>
       </div>
